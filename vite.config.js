323730d3bd0e14568e92dadf2bcd678dc4b6e011
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-16 15:41:13 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
+  },
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
